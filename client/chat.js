63371fb399997bec7f09cb93ef73815f8e4e1b67
Jan Pfeiffer <jan.pfeiffer@cosmoconsult.com>
2020-12-18 08:39:43 +0100

    .

--- client/chat.js
+++ client/chat.js
@@ -4,10 +4,10 @@ const config = {
 
 const messagesContainer = document.getElementById('messages')
 const formEl = document.getElementById('form')
-
-new window.EventSource(`${config.serverIP}/chat/sse`).onmessage = function (event) {
-  addMessage(JSON.parse(event.data))
-}
+const inputEl = document.getElementById('input')
+const nameEl = document.getElementById('chatName')
+const colorEl = document.getElementById('color')
+const submitEl = document.getElementById('submitBtn')
 
 function addMessage(message) {
   const date = new Date(message.createdAt)
@@ -21,57 +21,62 @@ function addMessage(message) {
     <span class="timestamp">${formattedTimestamp}</span>
   `
   messagesContainer.appendChild(messageEl)
-  messagesContainer.scrollTop = messagesContainer.scrollHeight
-
+  scrollBottom()
 }
 
-formEl.addEventListener('submit', (event) => {
-  evt.preventDefault()
-  submitMessage()
-})
-
-window.addEventListener('resize', () => messagesContainer.scrollTop = messagesContainer.scrollHeight)
-
 function submitMessage() {
-  if (!window.input.value) {
+  if (!inputEl.value) {
     return alert('Message is required!')
   }
-  window.fetch(`${config.serverIP}/chat`, {
+  fetch(`${config.serverIP}/chat`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ message: window.input.value, color: window.color.value, name: window.chatName.value })
+    body: JSON.stringify({ message: inputEl.value, color: colorEl.value, name: nameEl.value })
   })
-  window.input.value = ''
+  inputEl.value = ''
 }
 
-window.color.addEventListener('change', () => {
-  localStorage.setItem('chat-color', window.color.value);
+function scrollBottom() {
+  messagesContainer.scrollTop = messagesContainer.scrollHeight
+}
+
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault()
+  submitMessage()
 })
 
-window.chatName.addEventListener('change', () => {
-  localStorage.setItem('chat-name', window.chatName.value);
+window.addEventListener('resize', scrollBottom)
+
+colorEl.addEventListener('change', () => {
+  localStorage.setItem('chat-color', colorEl.value);
 })
 
-window.submitBtn.addEventListener('click', () => {
+nameEl.addEventListener('change', () => {
+  localStorage.setItem('chat-name', nameEl.value);
+})
+
+submitEl.addEventListener('click', () => {
   submitMessage()
 })
 
 document.addEventListener('DOMContentLoaded', () => {
   const localColor = localStorage.getItem('chat-color')
   const localName = localStorage.getItem('chat-name')
-  if (localColor) window.color.value = localColor
-  if (localName) window.chatName.value = localName
-  window.fetch(`${config.serverIP}/chat`).then(async res => {
+  if (localColor) colorEl.value = localColor
+  if (localName) nameEl.value = localName
+  fetch(`${config.serverIP}/chat`).then(async res => {
     const messages = await res.json();
     return messages
   }).then(messages => {
     messages.forEach(msg => {
       addMessage(msg)
     })
-    setTimeout(() => {
-      window.messages.scrollTop = window.messages.scrollHeight
-    }, 100);
+    setTimeout(scrollBottom, 100);
   })
+
+  new EventSource(`${config.serverIP}/chat/sse`).onmessage = function (event) {
+    addMessage(JSON.parse(event.data))
+  }
 })
